811d2f4475a13e24de1ea2b5c38c05c945eaa028
Max De Marzi <maxdemarzi@hotmail.com>
2012-03-07 13:38:01 -0600

    updated readme, creating graph

--- neotime.rb
+++ neotime.rb
@@ -3,9 +3,9 @@ require 'neography'
 require 'sinatra'
 require 'uri'
 
-require 'net-http-spy'
+#require 'net-http-spy'
 # Net::HTTP.http_logger_options = {:body => true}    # just the body
-Net::HTTP.http_logger_options = {:verbose => true} # see everything
+#Net::HTTP.http_logger_options = {:verbose => true} # see everything
 
 def generate_text(length = 8)
   chars = 'abcdefghjkmnpqrstuvwxyz'
@@ -19,8 +19,14 @@ def generate_time(from = Time.local(2000, 1, 1), to = Time.now)
   Time.at(from + rand * (to.to_f - from.to_f)).strftime('%Y-%m-%d')
 end
 
-def create_rel(x,y)
-  [:create_relationship, "wrote", "{#{x}}", "{#{y}}", {:date => generate_time}]
+def powerlaw(min=1,max=500,n=10)
+    max += 1
+    pl = ((max**(n+1) - min**(n+1))*rand() + min**(n+1))**(1.0/(n+1))
+    (max-1-pl.to_i)+min
+end
+
+def create_rel(from,to,start_date,end_date)
+  [:create_relationship, "wrote", "{#{from}}", "{#{to}}", {:date => generate_time(start_date,end_date)}]
 end
 
 def create_graph
@@ -28,14 +34,20 @@ def create_graph
   graph_exists = neo.get_node_properties(1)
   return if graph_exists && graph_exists['name']
   commands = []
+  names = []
+  50.times do 
+    t = [generate_time, generate_time].sort
+    names << {:name => generate_text, 
+              :joined_at => t[0], 
+              :last_seen_at => t[1]}
+  end
+      
+  commands = names.map{ |n| [:create_node, n]}
   
-  names = 100.times.collect{|x| generate_text}
-  commands = names.map{ |n| [:create_node, {"name" => n}]}
-  
-  names.each_index do |x| 
-    wrote = names.size.times.map{|y| y}
-    wrote.sample(rand(500)).each do |y|
-      commands << create_rel(x,y)    
+  names.each_index do |from| 
+    powerlaw.times do
+      to = rand(50)
+      commands << create_rel(from,to, Time.local(names[to][:joined_at]), Time.local(names[to][:last_seen_at]))    
     end
   end
   batch_result = neo.batch *commands
